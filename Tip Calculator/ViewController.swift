cf4b38f402a265fed168f356bfe9f5a85348eba8
Ellis Chang <ellischang@elliss-macbook-pro.local>
2019-01-18 00:29:44 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Ellis Chang on 1/18/19.
+//  Copyright © 2019 Ellis Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
